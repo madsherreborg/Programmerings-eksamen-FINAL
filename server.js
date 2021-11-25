49d67ecc0108e5a32ac827d606b0aaddb60f1f31
Mads Herreborg <mads.herreborg@gmail.com>
2021-11-25 13:43:43 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+// imports
+const fs = require("fs");
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+
+// Port
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Serveren lytter på port ${PORT}`);
+});
+
+//login 
+
+app.get("/login"), (req, res) => {
+    res.status(200).json('succes');
+}; w
