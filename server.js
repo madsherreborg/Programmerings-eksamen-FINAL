8d543efcf2a1f2b51c5830281c1177b72f2e6cf9
Mads Herreborg <mads.herreborg@gmail.com>
2021-11-29 16:02:54 +0100

    Endpoint created for signup

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 const fs = require("fs");
 
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors())
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -17,6 +19,13 @@ app.listen(PORT, () => {
 
 //login 
 
-app.get("/login"), (req, res) => {
+app.get('/login', (req, res) => {
     res.status(200).json('succes');
-};
+});
+
+// signup
+app.post('/signup', (req, res) => {
+    const name = req.body.name;
+    const password = req.body.password;
+}
+)
